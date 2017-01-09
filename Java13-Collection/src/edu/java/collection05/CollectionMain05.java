179ae2edc853b694b80ba951363a6370cd3467ec
sophia1023 <sophia5848@gmail.com>
2017-01-09 14:58:22 +0900

    collection 수업자료..... linkec list 등

---
+++ Java13-Collection/src/edu/java/collection05/CollectionMain05.java
@@ -0,0 +1,24 @@
+package edu.java.collection05;
+
+public class CollectionMain05 {
+
+	public static void main(String[] args) {
+		//Collection<E>
+		//└ set<E>
+		//		└ HashSet<E>, TreeSet<E>
+		
+		//	Set<E>
+		// 1. 데이터의 저장 순서가 중요하지 않음 -> 인덱스가 없음.
+		// 2. 중복된 데이터의 저장을 허용하지 않음.
+		// 예) {1,2,3} = {3,2,1}
+		// 예) {1,1,1,2,2,2,3,3,3} = {1,2,3}
+		
+
+		// HashSet<E>: 검색을 빠르게 하기 위한 Hash 알고리듬이 적용된 Set
+		// TreeSet<E>: 정렬을 빠르게 하기 위한 Tree 알고리듬이 적용된 Set
+
+		
+
+	}
+
+}
